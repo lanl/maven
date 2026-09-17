774f62088bd1d5cf06e3e6c1876d87754d4307a2
Vedant Iyer <vedantiyer@hotmail.com>
2026-09-17 13:41:51 -0500

    Fill ai model drop down if valid for that api key

--- maven/app.py
+++ maven/app.py
@@ -1549,7 +1549,8 @@ def update_ai_model_dialog():
 
     st.write(f"##### Please review security guidelines and Rules of Use at your input base URL: {url}")
 
-    selected_model = st.selectbox("val", list(models.keys()), label_visibility="collapsed", index=None,
+    selected_model = st.selectbox("val", list(models.keys()), label_visibility="collapsed",
+                    index=list(models).index(os.environ.get("AI_MODEL")[7:]) if os.environ.get("AI_MODEL") and os.environ.get("AI_MODEL") in models.keys() else None,
                                     key="update_ai_model_selection")
 
     if st.button("Save", type="primary", width="stretch"):
@@ -2034,7 +2035,8 @@ if not loaded_keys:
 
             st.write(f"##### Please review security guidelines and Rules of Use at your input base URL: {url}")
 
-            selected_model = st.selectbox("val", list(models.keys()), label_visibility="collapsed", index=None,
+            selected_model = st.selectbox("val", list(models.keys()), label_visibility="collapsed",
+                              index=list(models).index(os.environ.get("AI_MODEL")[7:]) if os.environ.get("AI_MODEL") and os.environ.get("AI_MODEL") in models.keys() else None,
                                         key="new_ai_model_selection")
             if st.button("Save"):
                 if selected_model is None:
